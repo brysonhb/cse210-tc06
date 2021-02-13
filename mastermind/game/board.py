895bbca1e2692837ee5dc76f26a694c05569ba85
Craig Wright <craigers.wright@gmail.com>
2021-02-12 19:57:10 -0700

    to_string method changes

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -9,16 +9,14 @@ class Board:
 
         pass
 
-    def to_string(self, p1_name, p2_name, p1_guess, p2_guess):
+    def to_string(self, p1_name, p2_name, p1_guess, p2_guess, hint):
         """
         Formats the content to a string so that it can be displayed on the screen without issue
         Takes the names from the Roster class as arguments
         :return: string
         """
-        string = self._spacer + "Player " + p1_name + ': ' + p1_guess + ', ' + hint
-        string +=
-
-        pass
+        string = self._spacer + "Player " + p1_name + ': ' + p1_guess + ', ' + hint + '\n'
+        string += "Player " + p2_name + ': ' + p2_guess + ', ' + hint + '\n' + self._spacer
 
     def print_guesses(self):
         """
