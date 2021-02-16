0237f6dc2fa5c823d9e35546c6ede5fd2ae910c9
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-16 16:51:09 -0700

    cleaned up code

--- mastermind/game/roster.py
+++ mastermind/game/roster.py
@@ -17,15 +17,3 @@ class Roster:
         self._players.append(player2)
         print()
         return self._players
-
-    def get_player_1(self):
-        """
-        This method just gets the value from player 1 and returns it.
-        """
-        return self.player1
-
-    def get_player_2(self):
-        """
-        This method gets the value from player 2 and returns it.
-        """
-        return self.player2
